121a0972e609ee4ddc4b573f963bdc9b4c774880
chilx <chiliangxiao>
2024-06-12 23:20:47 +0800

    模块

---
+++ chilx-service/service-sys/src/main/java/com/chilx/sys/controller/SysController.java
@@ -0,0 +1,11 @@
+package com.chilx.sys.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chilx
+ * @date 2024/06/12
+ **/
+@RestController
+public class SysController {
+}
---
+++ chilx-service/service-user/src/main/java/com/chilx/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.chilx.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chilx
+ * @date 2024/06/12
+ **/
+@RestController
+public class UserController {
+
+
+
+
+}
